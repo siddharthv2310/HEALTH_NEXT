5c3eb84140605177ecad3fa7f36335ededa8ede2
siddharth verma <sidverma384@gmail.com>
2026-09-09 00:34:43 +0530

    want to deploy on aws using docker and nginx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -6,7 +6,7 @@ export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
     
-     const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
     const[aToken,setAToken]=useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken'):'');
 
